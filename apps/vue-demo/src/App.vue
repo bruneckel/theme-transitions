125ee68665fe0d6e9c1bc14e8023368e0dd62f85
Bruno Neckel <neckel.bw@gmail.com>
2026-08-03 15:20:32 -0300

    style: reformat demo app with prettier defaults

--- apps/vue-demo/src/App.vue
+++ apps/vue-demo/src/App.vue
@@ -1,31 +1,36 @@
 <script setup lang="ts">
-import { useThemeTransition } from '@bruneckel/vue-theme-transitions';
-import { originFromEvent } from '@bruneckel/theme-transitions-core';
+import { useThemeTransition } from "@bruneckel/vue-theme-transitions";
+import { originFromEvent } from "@bruneckel/theme-transitions-core";
 
-const { theme, isAnimating, toggleTheme } = useThemeTransition({ variant: 'spread' });
+const { theme, isAnimating, toggleTheme } = useThemeTransition({
+  variant: "spread",
+});
 </script>
 
 <template>
-	<button :disabled="isAnimating" @click="toggleTheme({ origin: originFromEvent($event) })">
-		{{ theme }}
-	</button>
+  <button
+    :disabled="isAnimating"
+    @click="toggleTheme({ origin: originFromEvent($event) })"
+  >
+    {{ theme }}
+  </button>
 </template>
 
 <style>
 html {
-	background: #fff;
-	color: #111;
+  background: #fff;
+  color: #111;
 }
 
 html.dark {
-	background: #111;
-	color: #fff;
+  background: #111;
+  color: #fff;
 }
 
 body {
-	min-height: 100vh;
-	display: grid;
-	place-items: center;
-	margin: 0;
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  margin: 0;
 }
 </style>
