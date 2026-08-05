1264d5e09667007c586e8f5fb6638bd94b0cc0de
Bruno Neckel <neckel.bw@gmail.com>
2026-08-05 05:55:14 -0300

    chore: remove orphaned chevron icon from both demos

--- apps/react-demo/src/components/icons/IconChevronRight.tsx
+++
@@ -1,8 +0,0 @@
-import { BaseIcon } from './BaseIcon';
-import type { IconProps } from './BaseIcon';
-
-export const IconChevronRight = ({ size, ...props }: IconProps) => (
-	<BaseIcon size={size} {...props}>
-		<path d="m9 18 6-6-6-6" />
-	</BaseIcon>
-);
